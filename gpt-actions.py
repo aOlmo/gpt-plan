a5c52cff09226659a980eb17f80745dec0af3c09
aolmo <aolmoher@uccs.edu>
2021-03-04 12:02:48 -0700

    Improved code readability + changed to Curie engine
    - For complete TODO list, check first lines of file

--- gpt-actions.py
+++ gpt-actions.py
@@ -1,10 +1,10 @@
 import os
 import time
-import nltk
 import pickle
 import openai
 import random
 import re
+
 from nltk.stem import PorterStemmer, LancasterStemmer
 
 def get_acts_objs(acts_text):
@@ -37,10 +37,11 @@ def get_prec_rec_f1(true_list, pred_list):
 
 # TODO: Use threads for parallel execution
 # TODO: We can still finetune further making sure we include all actions from last sentence
-# TODO: Make Lancaster, Porter and none in same execution
+# TODO: Make data already in CSV format for better postprocessing + save outputs from
+# TODO: Put params in config file
 # ================= Params =================
 random.seed(42)
-gpt3_temp = 0.0 # Set to 0 for reproducibility
+gpt3_temp = 0.0  # Set to 0 for reproducibility
 gpt3_max_tokens = 150
 openai.api_key = os.environ["OPENAI_API_KEY"]
 n_examples = 2  # The amount of text cannot go over 2048
@@ -78,7 +79,7 @@ stemmers_data = {
 
 domain = cook
 cnt = n_runs
-for i in range(n_runs):
+for _ in range(n_runs):
     query = ""
     # Randomly sample text sequences from the domain
     samples = random.sample(domain, n_examples+1)
@@ -107,7 +108,7 @@ for i in range(n_runs):
     print("[+]: Sending query...")
     start = time.time()
     response = openai.Completion.create(
-        engine="davinci",
+        engine="curie",
         prompt=f_query,
         temperature=gpt3_temp,
         max_tokens=gpt3_max_tokens,
@@ -126,47 +127,37 @@ for i in range(n_runs):
         cnt -= 1
         continue
 
+    # Get just the actions
     gpt3_acts_text = text_response.split(tags[0])[0]
     pred_acts, pred_objs = get_acts_objs(gpt3_acts_text)
 
+    for i, (true_list, pred_list) in enumerate(zip([true_acts, true_objs], [pred_acts, pred_objs])):
+        name = "acts" if i == 0 else "objs"
+        for k in stemmers_data:
+            stemmer = stemmers_data[k]["stemmer"]
+            if stemmer is not None:
+                true_list_stem = [stemmer.stem(w) for w in true_list]
+                pred_list_stem = [stemmer.stem(w) for w in pred_list]
+            else:
+                true_list_stem, pred_list_stem = true_list, pred_list
+
+            list_precision, list_recall, list_f1 = get_prec_rec_f1(true_list_stem, pred_list_stem)
+
+            stemmers_data[k]["precision"][i] += list_precision
+            stemmers_data[k]["recall"][i] += list_recall
+            stemmers_data[k]["f1"][i] += list_f1
+
+            print("[{}-{}-{}]: Precision: {:.2f} | Recall: {:.2f} | F1: {:.2f}".format(
+                k, i, name, list_precision, list_recall, list_f1
+            ))
+
+print("\n\n[+]: Computing averages over {} runs\n".format(cnt))
+for i, name in enumerate(["acts", "objs"]):
     for k in stemmers_data:
-        stemmer = stemmers_data[k]["stemmer"]
-        if stemmer is not None:
-            true_acts_stem = [stemmer.stem(w) for w in true_acts]
-            pred_acts_stem = [stemmer.stem(w) for w in pred_acts]
-            true_objs_stem = [stemmer.stem(w) for w in true_objs]
-            pred_objs_stem = [stemmer.stem(w) for w in pred_objs]
-        else:
-            true_acts_stem, pred_acts_stem = true_acts, pred_acts
-            true_objs_stem, pred_objs_stem = true_objs, pred_objs
+        avg_precision = stemmers_data[k]["precision"][i]/cnt
+        avg_recall = stemmers_data[k]["recall"][i]/cnt
+        avg_f1 = stemmers_data[k]["f1"][i]/cnt
 
-        acts_precision, acts_recall, acts_f1 = get_prec_rec_f1(true_acts_stem, pred_acts_stem)
-        objs_precision, objs_recall, objs_f1 = get_prec_rec_f1(true_objs_stem, pred_objs_stem)
-
-        stemmers_data[k]["precision"][0] += acts_precision
-        stemmers_data[k]["recall"][0] += acts_recall
-        stemmers_data[k]["f1"][0] += acts_f1
-
-        stemmers_data[k]["precision"][1] += objs_precision
-        stemmers_data[k]["recall"][1] += objs_recall
-        stemmers_data[k]["f1"][1] += objs_f1
-
-        print("[{}-{}-acts]: "
-              "Precision: {:.2f} | Recall: {:.2f} | F1: {:.2f}".format(k, i, acts_precision, acts_recall, acts_f1))
-        print("[{}-{}-objs]: "
-              "Precision: {:.2f} | Recall: {:.2f} | F1: {:.2f}".format(k, i, objs_precision, objs_recall, objs_f1))
-
-for k in stemmers_data:
-    avg_precision = stemmers_data[k]["precision"][0]/cnt
-    avg_recall = stemmers_data[k]["recall"][0]/cnt
-    avg_f1 = stemmers_data[k]["f1"][0]/cnt
-
-    print("[{}-acts] Avg over {} runs: Precision: {:.4f} | Recall: {:.4f} | F1: {:.4f}".format(
-        k, cnt, avg_precision, avg_recall, avg_f1))
-
-    avg_precision = stemmers_data[k]["precision"][1] / cnt
-    avg_recall = stemmers_data[k]["recall"][1] / cnt
-    avg_f1 = stemmers_data[k]["f1"][1] / cnt
-
-    print("[{}-objs] Avg over {} runs: Precision: {:.4f} | Recall: {:.4f} | F1: {:.4f}".format(
-        k, cnt, avg_precision, avg_recall, avg_f1))
+        print("[{}-{}]: Precision: {:.4f} | Recall: {:.4f} | F1: {:.4f}".format(
+            k, name, avg_precision, avg_recall, avg_f1
+        ))
