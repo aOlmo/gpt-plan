f110c5f6289254a1eb9b348d570607aca591a659
aolmo <aolmoher@uccs.edu>
2021-03-04 10:47:31 -0700

    Stemmers now in place for calculations, still to do same for objects

--- gpt-actions.py
+++ gpt-actions.py
@@ -7,6 +7,34 @@ import random
 import re
 from nltk.stem import PorterStemmer, LancasterStemmer
 
+def get_acts_objs(acts_text):
+    acts = [act.split("(")[0].strip() for act in acts_text.split("),") if act.split("(")[0].strip() != ""]
+    objs_list = re.findall(r'\((.*?)\)', acts_text)
+
+    objs = []
+    for obj in objs_list:
+        objs += obj.split(",")
+
+    return acts, objs
+
+def get_prec_rec_f1(true_list, pred_list):
+    true_list_cpy = true_list[:]
+    totalTruth = len(true_list)
+    totalTagged = len(pred_list)
+    totalRight = 0
+
+    # Check if the current action is in the list of gt actions, if it is, remove from the truth list
+    for act in pred_list:
+        if act in true_list_cpy:
+            totalRight += 1
+            true_list_cpy.remove(act)
+
+    precision = totalRight / totalTagged
+    recall = totalRight / totalTruth
+    f1 = 2 * precision * recall / (precision + recall) if (precision + recall) > 0 else 0
+
+    return precision, recall, f1
+
 # TODO: Use threads for parallel execution
 # TODO: We can still finetune further making sure we include all actions from last sentence
 # TODO: Make Lancaster, Porter and none in same execution
@@ -19,36 +47,39 @@ n_examples = 2  # The amount of text cannot go over 2048
 tags = ["\n\nTEXT: \n", "\nACTIONS: \n"]
 n_runs = 10
 max_acts = 10
-lancaster_stem = LancasterStemmer()
-porter_stem = PorterStemmer()
-stemmer = porter_stem
 # ==========================================
 
-def get_acts_objs(acts_text):
-    acts = [act.split("(")[0].strip() for act in acts_text.split("),") if act.split("(")[0].strip() != ""]
-    objs_list = re.findall(r'\((.*?)\)', acts_text)
+wiki = pickle.load(open("EASDRL/data/wikihow_labeled_text_data.pkl", "rb"))
+win = pickle.load(open("EASDRL/data/win2k_labeled_text_data.pkl", "rb"))
+cook = pickle.load(open("EASDRL/data/cooking_labeled_text_data.pkl", "rb"))
+_ = pickle.load(open("EASDRL/data/cooking_dependency.pkl", "rb"))
 
-    objs = []
-    for obj in objs_list:
-        objs += obj.split(",")
+stemmers_data = {
+    "Raw": {
+        "stemmer": None,
+        "precision": 0,
+        "recall": 0,
+        "f1": 0
+    },
+    "Porter": {
+        "stemmer": PorterStemmer(),
+        "precision": 0,
+        "recall": 0,
+        "f1": 0
+    },
+    "Lancaster": {
+        "stemmer": LancasterStemmer(),
+        "precision": 0,
+        "recall": 0,
+        "f1": 0
+    }
+}
 
-    return acts, objs
-
-a = pickle.load(open("EASDRL/data/wikihow_labeled_text_data.pkl", "rb"))
-b = pickle.load(open("EASDRL/data/win2k_labeled_text_data.pkl", "rb"))
-c = pickle.load(open("EASDRL/data/cooking_labeled_text_data.pkl", "rb"))
-d = pickle.load(open("EASDRL/data/refined_cooking_data.pkl", "rb"))
-e = pickle.load(open("EASDRL/data/cooking_dependency.pkl", "rb"))
-
-c = a
-
-t_prec = 0
-t_rec = 0
-t_f1 = 0
+domain = cook
 cnt = n_runs
 for i in range(n_runs):
     query = ""
-    samples = random.sample(c, n_examples+1)
+    samples = random.sample(domain, n_examples+1)
     for sample in samples:
         query += tags[0]
         # Get the sentence number of the last action
@@ -68,7 +99,6 @@ for i in range(n_runs):
     true_acts_text.replace(",", "")
 
     true_acts, true_objs = get_acts_objs(true_acts_text)
-    true_acts_stem = [stemmer.stem(w) for w in true_acts]
 
     # ---------------- Send GPT3 query --------------
     print("[+]: Sending query...")
@@ -89,35 +119,31 @@ for i in range(n_runs):
     # Process response
     if tags[0] not in text_response:
         print("[-]: Tag not found in response, continuing...")
+        print(text_response)
         cnt -= 1
         continue
 
     gpt3_acts_text = text_response.split(tags[0])[0]
     pred_acts, pred_objs = get_acts_objs(gpt3_acts_text)
-    pred_acts_stem = [stemmer.stem(w) for w in pred_acts]
 
-    true_acts_cpy = true_acts_stem[:]
-    totalTruth = len(true_acts_stem)
-    totalTagged = len(pred_acts_stem)
-    totalRight = 0
-    # Check if the current action is in the list of gt actions, if it is, remove from the truth list
-    for act in pred_acts_stem:
-        if act in true_acts_cpy:
-            totalRight += 1
-            true_acts_cpy.remove(act)
+    for k in stemmers_data:
+        stemmer = stemmers_data[k]["stemmer"]
+        if stemmer is not None:
+            true_acts_stem = [stemmer.stem(w) for w in true_acts]
+            pred_acts_stem = [stemmer.stem(w) for w in pred_acts]
+        else:
+            true_acts_stem, pred_acts_stem = true_acts, pred_acts
 
-    precision = totalRight/totalTagged
-    recall = totalRight/totalTruth
-    f1 = 2*precision*recall/(precision+recall) if (precision+recall) > 0 else 0
-    print("[{}]: Precision: {:.2f} | Recall: {:.2f} | F1: {:.2f}\n".format(i, precision, recall, f1))
+        precision, recall, f1 = get_prec_rec_f1(true_acts_stem, pred_acts_stem)
+        stemmers_data[k]["precision"] += precision
+        stemmers_data[k]["recall"] += recall
+        stemmers_data[k]["f1"] += f1
 
-    t_prec += precision
-    t_rec += recall
-    t_f1 += f1
+        print("[{}-{}]: Precision: {:.2f} | Recall: {:.2f} | F1: {:.2f}".format(k, i, precision, recall, f1))
 
-t_prec /= n_runs
-t_rec /= n_runs
-t_f1 /= n_runs
-print("Avgs over {} runs:\nPrecision: {:.2f} | Recall: {:.2f} | F1: {:.2f}".format(cnt, t_prec, t_rec, t_f1))
-
-
+for k in stemmers_data:
+    avg_precision = stemmers_data[k]["precision"]/cnt
+    avg_recall = stemmers_data[k]["recall"]/cnt
+    avg_f1 = stemmers_data[k]["f1"]/cnt
+    print("[{}] Avg over {} runs: Precision: {:.4f} | Recall: {:.4f} | F1: {:.4f}".format(
+        k, cnt, avg_precision, avg_recall, avg_f1))
