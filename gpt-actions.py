1e6adc40ff0d14fe0a5dce55f2fdfff42cdf5b23
aolmo <aolmoher@asu.edu>
2021-11-26 17:23:18 -0700

    Training queries are now saved

--- gpt-actions.py
+++ gpt-actions.py
@@ -14,7 +14,7 @@ from tqdm import tqdm
 ############################ Threading ##############################
 
 class threadGPT3(threading.Thread):
-    def __init__(self, threadID, gpt3_engine, n_examples, max_sents, domain_examples, domain_examples_ids):
+    def __init__(self, threadID, gpt3_engine, n_examples, max_sents, domain_examples, domain_examples_ids, save_tr_queries=True):
         threading.Thread.__init__(self)
         self.threadId = threadID
         self.gpt3_engine = gpt3_engine
@@ -23,6 +23,7 @@ class threadGPT3(threading.Thread):
         self.domain_examples = domain_examples
         self.domain_examples_ids = domain_examples_ids
         self.results = {"precision": [0, 0], "recall": [0, 0], "f1": [0, 0]}
+        self.save_tr_queries = save_tr_queries
 
     def send_query_gpt3(self, query):
         max_token_err_flag = False
@@ -45,6 +46,7 @@ class threadGPT3(threading.Thread):
 
     def run(self):
         for domain_name in DOMAINS:
+            queries_file = open("data/queries_{}.txt".format(domain_name), "a+")
             out_file = open("data/{}_{}_{}.out".format(self.gpt3_engine, domain_name, self.n_examples), "a+")
             out_file.write(str(self.domain_examples_ids[domain_name]) + ": {} EXAMPLES\n".format(self.n_examples))
             domain = DOMAINS[domain_name]
@@ -59,6 +61,10 @@ class threadGPT3(threading.Thread):
                 cnt += 1
 
                 query = train_samples_str.strip()
+
+                if self.save_tr_queries:
+                    queries_file.write(query)
+                    queries_file.write("\n\n")
 
                 test_data = get_data(test_sample, max_sents=200)
                 text_str_test, _ = get_query_strs(test_data)
@@ -103,30 +109,31 @@ class threadGPT3(threading.Thread):
                 pbar.set_postfix({"f1_act": self.results["f1"][0] / cnt, "f1_obj": self.results["f1"][1] / cnt})
 
             ########### Averages ###########
-            print("ENGINE: {} | DOMAIN: {} | N_EXAMPLES: {}\n{}".format(self.gpt3_engine, domain_name, self.n_examples, "-" * 28))
-            print("[+]: AVERAGES over {} runs".format(cnt))
-            for i, name in enumerate(["acts", "objs"]):
-                for op in ["precision", "recall", "f1"]:
-                    self.results[op][i] /= cnt
-                    self.results[op][i] = round(self.results[op][i], 4)
+            if cnt > 0:
+                print("ENGINE: {} | DOMAIN: {} | N_EXAMPLES: {}\n{}".format(self.gpt3_engine, domain_name, self.n_examples, "-" * 28))
+                print("[+]: AVERAGES over {} runs".format(cnt))
+                for i, name in enumerate(["acts", "objs"]):
+                    for op in ["precision", "recall", "f1"]:
+                        self.results[op][i] /= cnt
+                        self.results[op][i] = round(self.results[op][i], 4)
 
-                print("[{}-exs-{}]: Precision: {:.4f} | Recall: {:.4f} | F1: {:.4f}".format(
-                    name, self.n_examples, self.results["precision"][i], self.results["recall"][i], self.results["f1"][i]
-                ))
+                    print("[{}-exs-{}]: Precision: {:.4f} | Recall: {:.4f} | F1: {:.4f}".format(
+                        name, self.n_examples, self.results["precision"][i], self.results["recall"][i], self.results["f1"][i]
+                    ))
 
-            prec, rec, f1 = self.results["precision"], self.results["recall"], self.results["f1"]
-            x = [list(DOMAINS.keys()).index(domain_name), ALL_ENGINES.index(self.gpt3_engine),
-                 cnt, self.n_examples, self.max_sents, prec[0], rec[0], f1[0], prec[1], rec[1], f1[1]]
+                prec, rec, f1 = self.results["precision"], self.results["recall"], self.results["f1"]
+                x = [list(DOMAINS.keys()).index(domain_name), ALL_ENGINES.index(self.gpt3_engine),
+                     cnt, self.n_examples, self.max_sents, prec[0], rec[0], f1[0], prec[1], rec[1], f1[1]]
 
-            ########### Save data ###########
-            print("[+]: Saving data \n\n")
-            cols = ["domain", "engine", "runs", "examples", "max_sents", "prec_acts", "rec_acts", "f1_acts",
-                    "prec_objs", "rec_objs", "f1_objs"]
-            new_data = pd.DataFrame(np.array(x).reshape(1, -1), columns=cols)
-            df = pd.read_csv(results_file).append(new_data) if os.path.exists(results_file) else new_data
-            df.to_csv(results_file, index=False)
-            #################################
-            out_file.write("\n\n")
+                ########### Save data ###########
+                print("[+]: Saving data \n\n")
+                cols = ["domain", "engine", "runs", "examples", "max_sents", "prec_acts", "rec_acts", "f1_acts",
+                        "prec_objs", "rec_objs", "f1_objs"]
+                new_data = pd.DataFrame(np.array(x).reshape(1, -1), columns=cols)
+                df = pd.read_csv(results_file).append(new_data) if os.path.exists(results_file) else new_data
+                df.to_csv(results_file, index=False)
+                #################################
+                out_file.write("\n\n")
 
 if __name__ == '__main__':
     wiki = pickle.load(open("data/datasets/wikihow_labeled_text_data.pkl", "rb"))
@@ -144,7 +151,7 @@ if __name__ == '__main__':
     gpt3_max_tokens = 100
     selected_engines = ["ada"]
 
-    results_file = "output.csv"
+    results_file = "results.csv"
     openai.api_key = "" #os.environ["OPENAI_API_KEY"]
     n_examples_list = [1, 2]  # 1: Random, 2: Excl+Opt 3: Es+Ex+Op 4: Random +prev
     count_unfinished_responses = True
