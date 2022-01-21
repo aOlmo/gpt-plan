7a70d4be973efa53936c0165bf90d127e2d44ffa
aolmo <aolmoher@asu.edu>
2022-01-21 11:28:29 -0700

    Improved text_to_plan parsing

--- gpt_planner/gen_text.py
+++ gpt_planner/gen_text.py
@@ -4,12 +4,12 @@ import numpy as np
 from pathlib import Path
 from tarski.io import PDDLReader
 
-nd = {"a": "Blue", "b": "Orange", "c": "Red", "d": "Yellow", "e": "White", "f": "Magenta"}
+nd = {"a": "blue", "b": "orange", "c": "red", "d": "yellow", "e": "white", "f": "magenta"}
 
 def plan_to_text():
     def treat_on(atom):
         terms = atom.subterms
-        return f"{nd[terms[0].name]} on {nd[terms[1].name]}"
+        return f"the {nd[terms[0].name]} block on top of the {nd[terms[1].name]} block"
 
     BLOCKS = [nd[x.name] for x in list(lang.constants())]
 
@@ -50,7 +50,6 @@ def plan_to_text():
         elif i < n - 1:
             GOAL += ", "
 
-
     plan_file = "sas_plan"
     PLAN = ""
     with open(plan_file) as f:
@@ -63,30 +62,57 @@ def plan_to_text():
         objs = [nd[x] for x in objs]
 
         on_from = " from " if "unstack" in act_name else " on "
-        PLAN += f'{act_name} {f"{on_from}".join(objs)}'
+        PLAN += f'{act_name} the {f"{on_from}".join(objs)} block' #TODO: Continue treating this 'on'
 
+        print(PLAN)
         if i == cost - 2:
             PLAN += " and "
         elif i < cost - 1:
             PLAN += ", "
 
     TEMPLATE = \
-        f"""
-        We have blocks {", ".join(BLOCKS[:-1])} and {BLOCKS[-1]}.
-        Block {INIT}.\nOur goal is to have {GOAL}.
-        
-        In order, the plan is to {PLAN}.
-        """
+        f"There is a {', '.join(BLOCKS[:-1])} and {BLOCKS[-1]} block on the table.\nMy goal is to have {GOAL}.\nMy plan is as follows \n {PLAN}."
 
     return TEMPLATE.replace("-", " ").replace("ontable", "on table")
 
 # NOTE: We are assuming:
-#       (1) Actions in the text we have them on the domain beforehand
+#       (1) Actions in the text we have them on the domain
 #       (2) The 'put' action is assumed to be 'put down'
 #       (3) We know the object names
 #       (4) Objects order are placed in the same order that appear in the sentence
-def text_to_plan(actions):
-    text = """
+def text_to_plan(text, action_set):
+    def get_ordered_objects(object_names, line):
+        objs = []
+        pos = []
+        for obj in object_names:
+            if obj in line:
+                objs.append(obj)
+                pos.append(line.index(obj))
+
+        sorted_zipped_lists = sorted(zip(pos, objs))
+        return [el for _, el in sorted_zipped_lists]
+
+    actions_params_dict = dict(action_set.items())
+    raw_actions = list(action_set.keys())
+    actions = [x.replace("-", " ") for x in action_set.keys()]
+
+    object_names = [x.lower() for x in nd.values()]
+    lines = [line.strip() for line in text.lower().strip().split("\n")]
+    plan = []
+    for line in lines:
+        action_list = [action in line for action in actions]
+        assert sum(action_list) == 1
+        action = raw_actions[np.where(action_list)[0][0]]
+        n_objs = len(actions_params_dict[action].parameters.vars())
+
+        objs = get_ordered_objects(object_names, line)
+
+        action = "({} {})".format(action, " ".join(objs[:n_objs+1]))
+        plan.append(action)
+
+    return plan
+
+TEXT = """
     Pick up the blue block from on top of the yellow block
     Put down the blue block on the table
     Pick up the yellow block from the table
@@ -95,39 +121,21 @@ def text_to_plan(actions):
     Stack the red block on top of the yellow block
     """
 
-    actions_params_dict = dict(actions.items())
-    raw_actions = list(actions.keys())
-    actions = [x.replace("-", " ") for x in actions.keys()]
-
-    object_names = [x.lower() for x in nd.values()]
-    lines = [line.strip() for line in text.lower().strip().split("\n")]
-    for line in lines:
-        action_list = [action in line for action in actions]
-        assert sum(action_list) == 1
-        action = raw_actions[np.where(action_list)[0][0]]
-        n_params = len(actions_params_dict[action].parameters.vars())
-        params = [x for x in object_names if re.search(x, line)]  # TODO: this should be in order
-
-        action = "({} {})".format(action, " ".join(params[:n_params+1]))
-        print(action)
-
-    #TODO: Use VAL here
-
-
+INTRO = """
+    I am playing with a set of blocks where I need to arrange the blocks into stacks. \
+    I can only pick up one block at a time and I can only pick up a block if there are no other blocks on top of it.
+    """
 
 if __name__ == '__main__':
     domain = './blocksworld.pddl'
     instance = './instances/instance-{}.pddl'
     plan_out = "sas_plan"
 
-    for i in range(1, 10):
+    for i in range(1, 2):
         cur_instance = instance.format(i)
         reader = PDDLReader(raise_on_error=True)
         reader.parse_domain(domain)
         problem = reader.parse_instance(cur_instance)
-
-        text_to_plan(reader.parser.problem.actions)
-        exit()
 
         cmd = f"~/soft/downward/fast-downward.py {domain} {cur_instance} --search \"astar(lmcut())\" > /dev/null 2>&1"
         os.system(cmd)
@@ -137,6 +145,8 @@ if __name__ == '__main__':
         print(plan_to_text())
 
 
+        # plan = text_to_plan(TEXT, problem.actions)
+        # validate_plan(plan, problem)
 
 ##############################
 # print(lang.sorts)
