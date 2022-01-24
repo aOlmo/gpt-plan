739a0b25adf769e5696dceee29865340ad5c7fc5
aolmo <aolmoher@asu.edu>
2022-01-24 11:51:32 -0700

    Possibly first working version of plan_to_text and text_to_plan functions, still to add GPT-3 calls

--- gpt_planner/gen_text.py
+++ gpt_planner/gen_text.py
@@ -4,14 +4,15 @@ import numpy as np
 from pathlib import Path
 from tarski.io import PDDLReader
 
-nd = {"a": "blue", "b": "orange", "c": "red", "d": "yellow", "e": "white", "f": "magenta"}
+ND = {"a": "blue", "b": "orange", "c": "red", "d": "yellow", "e": "white", "f": "magenta"}
+DN = {v: k for k, v in ND.items()}
 
-def plan_to_text():
+def plan_to_text(get_plan=True):
     def treat_on(atom):
         terms = atom.subterms
-        return f"the {nd[terms[0].name]} block on top of the {nd[terms[1].name]} block"
+        return f"the {ND[terms[0].name]} block on top of the {ND[terms[1].name]} block"
 
-    BLOCKS = [nd[x.name] for x in list(lang.constants())]
+    BLOCKS = [ND[x.name] for x in list(lang.constants())]
 
     data = {}
     for atom in problem.init.as_atoms():
@@ -21,25 +22,27 @@ def plan_to_text():
 
         if pred_name == "on":
             first, second = atom.subterms
-            data["on"].append(f"{nd[first.name]} is on {nd[second.name]}")
+            data["on"].append(f"the {ND[first.name]} block is on top of the {ND[second.name]} block")
         elif atom.subterms:
-            data[pred_name].append(nd[atom.subterms[0].name])
+            data[pred_name].append(ND[atom.subterms[0].name])
 
-    n = len(data)
+    # We delete these for GPT-3
+    del data['clear']
+    del data['handempty']
     INIT = ""
+    n = len(data)
     for i, obj in enumerate(data):
         n_obj = len(data[obj])
         if obj == "on":
-            a = " and " if len(data[obj]) > 1 else ""
-            INIT += ", ".join(data[obj][:-1]) + f"{a}{data[obj][-1]}"
+            _and = " and " if len(data[obj]) > 1 else ""
+            string = ", ".join(data[obj][:-1]) + f"{_and}{data[obj][-1]}"
+            INIT += string.capitalize()
         else:
-            if n_obj >= 1:
-                a = " and " if len(data[obj]) > 1 else ""
-                is_are = "are" if n_obj > 1 else "is"
-                INIT += ", ".join(data[obj][:-1]) + f"{a}{data[obj][-1]}" + f" {is_are} {obj}"
-            else:
-                INIT += f"we have {obj}"
-        if i < n - 1: INIT += " and "
+            _and = " and " if len(data[obj]) > 1 else ""
+            is_are = "are" if n_obj > 1 else "is"
+            string = "the " + ", ".join(data[obj][:-1]) + f"{_and}{data[obj][-1]} block{'s' if n_obj > 1 else ''}" + f" {is_are} {obj}"
+            INIT += string.capitalize()
+        INIT += ". "
 
     GOAL = ""
     n = len(problem.goal.subformulas)
@@ -52,28 +55,32 @@ def plan_to_text():
 
     plan_file = "sas_plan"
     PLAN = ""
-    with open(plan_file) as f:
-        plan = [line.rstrip() for line in f][:-1]
+    if get_plan:
+        PLAN = "\n\n"
+        with open(plan_file) as f:
+            plan = [line.rstrip() for line in f][:-1]
 
-    cost = len(plan)
-    for i, action in enumerate(plan):
-        action = action.strip("(").strip(")")
-        act_name, objs = action.split(" ")[0], action.split(" ")[1:]
-        objs = [nd[x] for x in objs]
+        cost = len(plan)
+        for i, action in enumerate(plan):
+            action = action.strip("(").strip(")")
+            act_name, objs = action.split(" ")[0], action.split(" ")[1:]
+            objs = [ND[x] for x in objs]
 
-        on_from = " from " if "unstack" in act_name else " on "
-        PLAN += f'{act_name} the {f"{on_from}".join(objs)} block' #TODO: Continue treating this 'on'
+            on_from = "from" if "unstack" in act_name else "on top of"
+            if len(objs) == 2:
+                PLAN += f'{act_name.capitalize()} the {objs[0]} block {on_from} the {objs[1]} block'
+            elif len(objs) == 1:
+                PLAN += f'{act_name.capitalize()} the {objs[0]} block'
 
-        print(PLAN)
-        if i == cost - 2:
-            PLAN += " and "
-        elif i < cost - 1:
-            PLAN += ", "
+            if i == cost - 2:
+                PLAN += " and "
+            elif i < cost - 1:
+                PLAN += "\n"
+        PLAN += ".\nEnd of plan.\n\n"
+    TEMPLATE = f"{INIT.strip()}\nMy goal is to have {GOAL}.\nMy plan is as follows{PLAN}"
 
-    TEMPLATE = \
-        f"There is a {', '.join(BLOCKS[:-1])} and {BLOCKS[-1]} block on the table.\nMy goal is to have {GOAL}.\nMy plan is as follows \n {PLAN}."
+    return TEMPLATE.replace("-", " ").replace("ontable", "on the table")
 
-    return TEMPLATE.replace("-", " ").replace("ontable", "on table")
 
 # NOTE: We are assuming:
 #       (1) Actions in the text we have them on the domain
@@ -96,9 +103,10 @@ def text_to_plan(text, action_set):
     raw_actions = list(action_set.keys())
     actions = [x.replace("-", " ") for x in action_set.keys()]
 
-    object_names = [x.lower() for x in nd.values()]
+    object_names = [x.lower() for x in ND.values()]
     lines = [line.strip() for line in text.lower().strip().split("\n")]
-    plan = []
+
+    plan = ""
     for line in lines:
         action_list = [action in line for action in actions]
         assert sum(action_list) == 1
@@ -106,19 +114,21 @@ def text_to_plan(text, action_set):
         n_objs = len(actions_params_dict[action].parameters.vars())
 
         objs = get_ordered_objects(object_names, line)
+        objs = [DN[x] for x in objs]
 
-        action = "({} {})".format(action, " ".join(objs[:n_objs+1]))
-        plan.append(action)
+        action = "({} {})".format(action, " ".join(objs[:n_objs + 1]))
+        plan += f"{action}\n"
 
     return plan
 
+
 TEXT = """
-    Pick up the blue block from on top of the yellow block
-    Put down the blue block on the table
-    Pick up the yellow block from the table
-    Stack the yellow block on top of the blue block
-    Pick up the red block from the table
-    Stack the red block on top of the yellow block
+Pick up the orange block
+Stack the orange block on top of the blue block
+Pick up the red block
+Stack the red block on top of the orange block
+Pick up the yellow block 
+Stack the yellow block on top of the red block.
     """
 
 INTRO = """
@@ -131,19 +141,29 @@ if __name__ == '__main__':
     instance = './instances/instance-{}.pddl'
     plan_out = "sas_plan"
 
-    for i in range(1, 2):
+    n_examples = 3
+    query = ""
+    for i in [2]:#range(1, 2+n_examples):
         cur_instance = instance.format(i)
+        print(f"Instance {cur_instance}")
         reader = PDDLReader(raise_on_error=True)
         reader.parse_domain(domain)
         problem = reader.parse_instance(cur_instance)
+
+        # plan = text_to_plan(TEXT, problem.actions)
+        # print(plan)
 
         cmd = f"~/soft/downward/fast-downward.py {domain} {cur_instance} --search \"astar(lmcut())\" > /dev/null 2>&1"
         os.system(cmd)
 
         plan_file = Path(plan_out).read_text()
         lang = problem.language
-        print(plan_to_text())
+        last_plan = False if i == n_examples+1 else True
+        query += plan_to_text(last_plan)
+        query += "===================\n"
 
+    print("== Query ==")
+    print(query)
 
         # plan = text_to_plan(TEXT, problem.actions)
         # validate_plan(plan, problem)
