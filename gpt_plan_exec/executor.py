68d46c540a0cb62666565831dd17930d52b7ee0e
Karthik Valmeekam (Student) <karthikv792@gmail.com>
2022-02-14 14:16:59 -0700

    translation_in_progress

--- gpt_plan_exec/executor.py
+++ gpt_plan_exec/executor.py
@@ -9,13 +9,18 @@ from model_parser.writer_new import ModelWriter
 from model_parser.constants import *
 import os
 import random
-class executor():
-    def __init__(self,domain,problem):
-        self.pr_domain, self.pr_problem = self.ground_domain(domain,problem)
-        self.model = parse_model(self.pr_domain,self.pr_problem)
-        self.plan,self.init_state,self.final_state,self.all_preds,self.not_true_preds,self.goal_state,self.prefix, self.replanning = [None]*8
 
-    def replanning(self,harder = 0):
+
+class executor():
+    def __init__(self, domain, problem):
+        self.pr_domain, self.pr_problem = self.ground_domain(domain, problem)
+        self.model = parse_model(self.pr_domain, self.pr_problem)
+        self.plan = self.get_plan(self.pr_domain, self.pr_problem)
+        self.init_state = self.get_sets(self.model[INSTANCE][INIT][PREDICATES])
+        self.goal_state = self.get_sets(self.model[INSTANCE][GOAL])
+        self.final_state, self.all_preds, self.not_true_preds, self.prefix, self.replanning_init = [None] * 5
+
+    def replanning(self, harder=0):
         """
         1. Execute a random prefix of a plan and get the resulting state
         2. Regress the suffix of the plan from the goal and get the resulting (partial) state
@@ -28,23 +33,20 @@ class executor():
         regress_state = self.regress(harder)
         if harder:
             this_much_harder = random.choice(range(len(regress_state)))
-            to_remove = set(random.choices(list(regress_state),k=this_much_harder))
-            self.replanning = self.final_state.difference(to_remove)
+            to_remove = set(random.choices(list(regress_state), k=this_much_harder))
+            self.replanning_init = self.final_state.difference(to_remove)
         else:
             this_much_easier = random.choice(range(len(regress_state)))
-            to_add = set(random.choices(list(regress_state),k=this_much_easier))
-            self.replanning = self.final_state.union(to_add)
+            to_add = set(random.choices(list(regress_state), k=this_much_easier))
+            self.replanning_init = self.final_state.union(to_add)
 
-
-
-
-
-    def regress(self,harder):
+    def regress(self, harder):
         curr_state = self.goal_state
         suffix = self.plan[self.prefix:][::-1]
         if harder:
             for act in suffix:
-                #goal - effects u precond
+                act = act.upper()
+                # goal - effects u precond
                 act_adds = self.get_sets(self.model[DOMAIN][act][ADDS])
                 act_dels = self.get_sets(self.model[DOMAIN][act][DELS])
                 act_pos_precs = self.get_sets(self.model[DOMAIN][act][POS_PREC])
@@ -57,6 +59,7 @@ class executor():
         else:
             rand_suff = random.choice(range(len(suffix)))
             for act in suffix[:rand_suff]:
+                act = act.upper()
                 act_adds = self.get_sets(self.model[DOMAIN][act][ADDS])
                 act_dels = self.get_sets(self.model[DOMAIN][act][DELS])
                 act_pos_precs = self.get_sets(self.model[DOMAIN][act][POS_PREC])
@@ -69,23 +72,25 @@ class executor():
 
         return curr_state
 
-
-
     def random_prefix_execution(self):
-        self.plan, self.prefix = self.get_plan(self.pr_domain,self.pr_problem)
-        self.init_state = self.get_sets(self.model[INSTANCE][INIT][PREDICATES])
-        # print(self.model[INSTANCE][INIT][PREDICATES])
-        # for act in self.model[DOMAIN]:
-        #     print(act==self.plan[0])
-        #     print(self.model[DOMAIN][act][ADDS])
-        #     print(self.model[DOMAIN][act][DELS])
+        self.prefix = random.choice(list(range(len(self.plan))))
         self.final_state = self.get_final_state()
         self.all_preds = self.get_sets(self.model[PREDICATES])
         self.not_true_preds = self.all_preds.difference(self.final_state)
-        self.goal_state =  self.get_sets(self.model[INSTANCE][GOAL])
 
+    def get_final_state(self, ):
+        initial_state = self.init_state
 
-    def get_plan(self,domain,problem):
+        for act in self.plan[:self.prefix]:
+            act = act.upper()
+            act_adds = self.get_sets(self.model[DOMAIN][act][ADDS])
+            act_dels = self.get_sets(self.model[DOMAIN][act][DELS])
+            initial_state = initial_state.union(act_adds)
+            initial_state = initial_state.difference(act_dels)
+
+        return initial_state
+
+    def get_plan(self, domain, problem):
         """
         Executes FD and returns a random prefix of the plan
         :param domain:
@@ -94,31 +99,18 @@ class executor():
         """
         CMD_FD = f"~/RADAR-X/planner/FAST-DOWNWARD/fast-downward.py {domain} {problem} --search \"astar(lmcut())\" >/dev/null 2>&1"
         os.system(CMD_FD)
-        #USE SAS PLAN to get actions
+        # USE SAS PLAN to get actions
         plan = []
         with open('sas_plan') as f:
             for line in f:
                 if ';' not in line:
-                    plan.append((line.strip()[1:-1].strip()).upper())
-        prefix = random.choice(list(range(len(plan))))
-        return (plan,prefix)
-    def get_sets(self,list_of_preds):
+                    plan.append((line.strip()[1:-1].strip()))
+        return plan
+
+    def get_sets(self, list_of_preds):
         return set([i[0] for i in list_of_preds])
 
-    def get_final_state(self,):
-        initial_state = self.init_state
-
-        for act in self.plan[:self.prefix]:
-            act_adds = self.get_sets(self.model[DOMAIN][act][ADDS])
-            act_dels = self.get_sets(self.model[DOMAIN][act][DELS])
-            initial_state = initial_state.union(act_adds)
-            initial_state = initial_state.difference(act_dels)
-
-        return initial_state
-
-
-
-    def ground_domain(self,domain,problem):
+    def ground_domain(self, domain, problem):
         CMD_PR2 = f"~/RADAR-X/planner/PR2/pr2plan -d {domain}  -i {problem} -o blank_obs.dat >/dev/null 2>&1"
         os.system(CMD_PR2)
         pr_domain = 'pr-domain.pddl'
@@ -126,7 +118,7 @@ class executor():
         self.remove_explain(pr_domain, pr_problem)
         return pr_domain, pr_problem
 
-    def remove_explain(self,domain, problem):
+    def remove_explain(self, domain, problem):
         try:
             cmd = 'cat {0} | grep -v "EXPLAIN" > pr-problem.pddl.tmp && mv pr-problem.pddl.tmp {0}'.format(problem)
             os.system(cmd)
@@ -134,16 +126,17 @@ class executor():
             os.system(cmd)
         except:
             raise Exception('[ERROR] Removing "EXPLAIN" from pr-domain and pr-problem files.')
-if __name__=="__main__":
+
+
+if __name__ == "__main__":
     domain = 'instances/ipc_domain.pddl'
     problem = 'instances/instance-1.pddl'
-    exec = executor(domain,problem)
-    exec.replanning()
-    # for i in range(5):
-    #     print("\n")
-    #     exec.random_prefix_execution()
-    #     print("PLAN: ", exec.plan)
-    #     print("INITIAL STATE: ", exec.init_state)
-    #     print("After Plan Execution (A.P.E.) STATE: ", exec.final_state)
-    #     print("GOAL STATE: ", exec.goal_state)
-    #     print("NOT TRUE PREDS: ", exec.not_true_preds)
+    exec = executor(domain, problem)
+    for i in range(5):
+        print("\n")
+        exec.random_prefix_execution()
+        print("PLAN: ", exec.plan)
+        print("INITIAL STATE: ", exec.init_state)
+        print("After Plan Execution (A.P.E.) STATE: ", exec.final_state)
+        print("GOAL STATE: ", exec.goal_state)
+        print("NOT TRUE PREDS: ", exec.not_true_preds)
--- gpt_plan_exec/gpt_plan_exec.py
+++ gpt_plan_exec/gpt_plan_exec.py
@@ -13,9 +13,12 @@ We then translate the response back into PDDL and check if the response exists i
 
 import os
 import numpy as np
+import yaml
+
 import utils
 from utils import *
 from tarski.io import PDDLReader
+from executor import *
 np.random.seed(42)
 
 
@@ -32,8 +35,62 @@ I can only unstack a block from on top of another block if the block I am unstac
 I can only stack a block on top of another block if I had previously picked up or unstacked the block being stacked.
 I can only stack a block on top of another block if the block onto which I am stacking the block has no other blocks on top of it.
 """
+
+def plan_execution(exec, DATA):
+    """
+    We need
+        i. Initial State
+       ii. Plan subset
+      iii. Resulting state
+    If prompt:
+        Give Initial State, Plan Subset, a question regarding a pred in the resulting state and the answer
+    else:
+        Give Initial State, Plan Subset, a question regarding a pred in the resulting state
+    :return:
+    """
+    initial_state = exec.init_state
+    exec.random_prefix_execution()
+    plan_prefix = exec.plan[:exec.prefix]
+    resulting_state = exec.final_state
+    for i in initial_state:
+        pred = i.split('_')
+        #Specific to blocksworld
+        if pred[0]=="clear" or pred[0]=="handempty":
+            continue
+
+
+
+def generate_plan_subset():
+    """
+    We need
+        i. Initial State
+       ii. Plan subset
+      iii. Resulting state
+    If prompt:
+        Give Initial State, Plan Subset and Resulting State as Goal State
+    else:
+        Give Initial State and Resulting State as Goal State.
+    :return:
+    """
+    initial_state = exec.init_state
+    exec.random_prefix_execution()
+    plan_prefix = exec.plan[:exec.prefix]
+    resulting_state = exec.final_state
+
+def replanning_harder_easier():
+    """
+
+    :return:
+    """
+    exec.replanning()
+    initial_state = exec.replanning_init
+    plan_prefix = exec.plan[:exec.prefix]
+    goal_state = exec.goal_state
+
 N=20
 if __name__=="__main__":
+    with open('config.yaml', 'r') as file:
+        DATA = yaml.safe_load(file)
     #Generate blocksworld problems
     DOMAIN_TYPE = "ipc"
     #gen_blocksworld_problems([4,5],N+10)
@@ -56,23 +113,19 @@ if __name__=="__main__":
             last_plan = True if i == start + n_examples else False
             # --------------- Read Instance --------------- #
             cur_instance = instance.format(i)
-            reader = PDDLReader(raise_on_error=True)
-            reader.parse_domain(domain)
-            problem = reader.parse_instance(cur_instance)
-            lang = problem.language
+            exec = executor(domain,cur_instance)
+            """
+            1. Executing Plans
+            2. Generating a subset of a plan
+            3. Replanning
+            """
             print(f"Instance {cur_instance}")
             # --------------------------------------------- #
 
             # ------------ Put plan and instance into text ------------ #
             query += "\n[STATEMENT]\n"
-            plan = compute_plan(domain, cur_instance, plan_file)
-            try:
-                goal = ''.join((treat_on(utils.LETTERS_DICT,atom))[0] for atom in problem.goal.subformulas)
-            except:
-                goal = ''.join((treat_on(utils.LETTERS_DICT,atom))[0])
-            inst_text,answer = instance_to_text_blocksworld(DOMAIN_TYPE, problem, not last_plan)
-
-            query+=inst_text
+            # inst_text,answer = instance_to_text_blocksworld(DOMAIN_TYPE, problem, not last_plan)
+            # query+=inst_text
             # --------------------------------------------------------- #
         # print("QUERY------------\n",query)
         # print(answer)
